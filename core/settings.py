f8f2d8d5a2fe198ac9ec21a4df92426e16d2a734
Joao Batista <joaobldss@gmail.com>
2025-08-01 16:46:04 -0300

    feat: adicionando docker no projeto

--- core/settings.py
+++ core/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
@@ -131,5 +135,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CELERY_BROKER_URL = 'pyamqp://guest@localhost//' 
+CELERY_BROKER_URL = 'pyamqp://guest@rabbitmq//'
 CELERY_TIMEZONE = 'America/Sao_Paulo'
